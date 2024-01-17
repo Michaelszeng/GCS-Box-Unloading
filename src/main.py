35b58113ae7d01e6f88b16251a779206f30012db
Michaelszeng <michaelszeng@gmail.com>
2024-01-17 15:26:59 -0500

    fixed floating boxes error

--- src/main.py
+++ src/main.py
@@ -8,6 +8,8 @@ from pydrake.all import (
     RigidTransform,
     MultibodyPlant,
     RotationMatrix,
+    RollPitchYaw,
+    SpatialVelocity,
     ConstantVectorSource,
     AbstractValue,
     ContactModel
@@ -40,9 +42,9 @@ seed = int(args.randomization)
 ##### Settings #####
 close_button_str = "Close"
 this_drake_module_name = "cwd"
-box_randomization_runtime = 0.5
-sim_runtime = box_randomization_runtime + 0.5
-NUM_BOXES = 1
+box_randomization_runtime = 3
+sim_runtime = box_randomization_runtime + 1
+NUM_BOXES = 5
 
 np.random.seed(seed)
 
@@ -66,18 +68,17 @@ for i in range(NUM_BOXES):
     relative_path_to_box = '../data/Box_0_5_0_5_0_5.sdf'
     absolute_path_to_box = os.path.abspath(relative_path_to_box)
 
-    box_pos_x = np.random.uniform(-1.5, 1.5, 1)
-    box_pos_y = np.random.uniform(-1, 1, 1)
-    box_pos_z = np.random.uniform(0, 5, 1)
-
     box_directives += f"""
 - add_model: 
     name: Box_{i}
     file: file://{absolute_path_to_box}
-    default_free_body_pose:
+"""
+    
+    """
+        default_free_body_pose:
         Box_0_5_0_5_0_5:
             translation: [{box_pos_x[0]}, {box_pos_y[0]}, {box_pos_z[0]}]
-"""
+            rotation: !Rpy {{ deg: [0, 0, 0] }}"""
     
 scenario = add_directives(scenario, data=box_directives)
 
@@ -138,7 +139,32 @@ robot_joint_idx = plant.GetJointIndices(robot_model_idx)[0]  # JointIndex object
 robot_joint = plant.get_joint(robot_joint_idx)  # Joint object
 robot_joint.Lock(plant_context)
 
+# Set poses for all boxes
+for i in range(NUM_BOXES):
+    box_model_idx = plant.GetModelInstanceByName(f"Box_{i}")  # ModelInstanceIndex
+    box_body_idx = plant.GetBodyIndices(box_model_idx)[0]  # BodyIndex
+
+    box_pos_x = np.random.uniform(-1.5, 1.5, 1)
+    box_pos_y = np.random.uniform(-1, 1, 1)
+    box_pos_z = np.random.uniform(2, 5, 1)
+    plant.SetFreeBodyPose(plant_context, plant.get_body(box_body_idx), RigidTransform([box_pos_x[0], box_pos_y[0], box_pos_z[0]]))
+
 simulator.AdvanceTo(box_randomization_runtime)
+
+# Set a pos-x velocity for all boxes to shove them against the back of the truck trailer
+for i in range(NUM_BOXES):
+    box_model_idx = plant.GetModelInstanceByName(f"Box_{i}")  # ModelInstanceIndex
+    box_body_idx = plant.GetBodyIndices(box_model_idx)[0]  # BodyIndex
+    box_rigid_body = plant.GetRigidBodyByName("Box_0_5_0_5_0_5", box_model_idx)  # RigidBody
+
+    box_current_pose = plant.GetFreeBodyPose(plant_context, box_rigid_body)
+    print(box_current_pose)
+
+    print(f"velocity: {plant.GetVelocities(plant_context, box_model_idx)}")
+    box_velocity = plant.GetVelocities(plant_context, box_model_idx)
+    box_velocity_desired = box_velocity + np.array([0,0,-1,0,0,0])
+
+    plant.SetFreeBodySpatialVelocity(plant.get_body(box_body_idx), SpatialVelocity(box_velocity_desired[:3], box_velocity_desired[3:]), plant_context)
 
 # Put Top of truck trailer back and lock it
 # plant.SetFreeBodyPose(plant_context, plant.get_body(trailer_roof_body_idx), RigidTransform([0,0,0]))
