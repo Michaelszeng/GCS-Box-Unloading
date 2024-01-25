a6fa89ffff03a0be4a6319f3cd7da5abd4a294df
Michaelszeng <michaelszeng@gmail.com>
2024-01-25 11:13:50 -0500

    working clique covers

--- src/iris.py
+++ src/iris.py
@@ -83,65 +83,66 @@ def generate_source_iris_regions(meshcat, robot_pose, visualize_iris_scene=True)
     """
 
     # # Create new MBP containing just robot and truck trailer walls
-    builder = DiagramBuilder()
-    plant, scene_graph = AddMultibodyPlantSceneGraph(builder, time_step=0.001)
-    parser = Parser(plant)
-    model_instances = parser.AddModelsFromString(scenario_yaml_for_source_regions, ".dmd.yaml")
-    print(f"model_instances: {model_instances}")
-    plant.WeldFrames(plant.world_frame(), plant.GetFrameByName("base_link", plant.GetModelInstanceByName("robot_base")), robot_pose)
-    plant.Finalize()
-    if visualize_iris_scene:
-        meshcat2 = StartMeshcat()
-        visualizer = MeshcatVisualizer.AddToBuilder(builder, scene_graph, meshcat2)
-    diagram = builder.Build()
-    context = diagram.CreateDefaultContext()
-    plant_context = plant.GetMyContextFromRoot(context)
-    if visualize_iris_scene:
-        simulator = Simulator(diagram)
-        simulator.AdvanceTo(0.1)
-
-    # Run IRIS
-    regions = []
-    options = IrisOptions()
-    options.num_collision_infeasible_samples = 1
-    options.require_sample_point_is_contained = True
-    options.iteration_limit = 1  # TEMPORARY
-    region = IrisInConfigurationSpace(plant, plant_context, options)
-
-    regions.append(region)
-
-    test_iris_region(plant, plant_context, meshcat, regions)
-
-
-
-    # params = dict(edge_step_size=0.125)
-    # builder = RobotDiagramBuilder()
-    # diagram_builder = builder.builder()
-    # params["robot_model_instances"] = builder.parser().AddModelsFromString(scenario_yaml_for_source_regions, ".dmd.yaml")
-    # plant = builder.plant()
-    # scene_graph = builder.scene_graph()
+    # builder = DiagramBuilder()
+    # plant, scene_graph = AddMultibodyPlantSceneGraph(builder, time_step=0.001)
+    # parser = Parser(plant)
+    # model_instances = parser.AddModelsFromString(scenario_yaml_for_source_regions, ".dmd.yaml")
+    # print(f"model_instances: {model_instances}")
     # plant.WeldFrames(plant.world_frame(), plant.GetFrameByName("base_link", plant.GetModelInstanceByName("robot_base")), robot_pose)
+    # plant.Finalize()
     # if visualize_iris_scene:
     #     meshcat2 = StartMeshcat()
-    #     visualizer = MeshcatVisualizer.AddToBuilder(diagram_builder, scene_graph, meshcat2)
+    #     visualizer = MeshcatVisualizer.AddToBuilder(builder, scene_graph, meshcat2)
     # diagram = builder.Build()
-    # params["model"] = diagram
     # context = diagram.CreateDefaultContext()
     # plant_context = plant.GetMyContextFromRoot(context)
-    # checker = SceneGraphCollisionChecker(**params)
+    # if visualize_iris_scene:
+    #     simulator = Simulator(diagram)
+    #     simulator.AdvanceTo(0.1)
 
-    # options = IrisFromCliqueCoverOptions()
-    # options.num_builders = 7  # set to 1 fewer than number of cores on computer
-    # options.num_points_per_coverage_check = 1000
-    # options.num_points_per_visibility_round = 500  # 1000
-    # options.coverage_termination_threshold = 0.2  # set low threshold at first for faster debugging
+    # # Run IRIS
+    # regions = []
+    # options = IrisOptions()
+    # options.num_collision_infeasible_samples = 1
+    # options.require_sample_point_is_contained = True
+    # options.iteration_limit = 1  # TEMPORARY
+    # region = IrisInConfigurationSpace(plant, plant_context, options)
 
-    # sets = IrisInConfigurationSpaceFromCliqueCover(
-    #     checker=checker, options=options, sets=[]
-    # )
+    # regions.append(region)
 
-    # print(f"sets: {sets}")
-    # print(type(sets))
+    # test_iris_region(plant, plant_context, meshcat, regions)
 
-    # test_iris_region(plant, plant_context, meshcat, sets)
+
+
+    params = dict(edge_step_size=0.125)
+    robot_diagram_builder = RobotDiagramBuilder()
+    diagram_builder = robot_diagram_builder.builder()
+    params["robot_model_instances"] = robot_diagram_builder.parser().AddModelsFromString(scenario_yaml_for_source_regions, ".dmd.yaml")
+    plant = robot_diagram_builder.plant()
+    scene_graph = robot_diagram_builder.scene_graph()
+    plant.WeldFrames(plant.world_frame(), plant.GetFrameByName("base_link", plant.GetModelInstanceByName("robot_base")), robot_pose)
+    if visualize_iris_scene:
+        meshcat2 = StartMeshcat()
+        visualizer = MeshcatVisualizer.AddToBuilder(diagram_builder, scene_graph, meshcat2)
+    diagram = robot_diagram_builder.Build()
+    params["model"] = diagram
+    context = diagram.CreateDefaultContext()
+    plant_context = plant.GetMyContextFromRoot(context)
+    checker = SceneGraphCollisionChecker(**params)
+
+    options = IrisFromCliqueCoverOptions()
+    options.num_builders = 7  # set to 1 fewer than number of cores on computer
+    options.num_points_per_coverage_check = 1000
+    options.num_points_per_visibility_round = 100  # 1000
+    options.coverage_termination_threshold = 0.2  # set low threshold at first for faster debugging
+    options.minimum_clique_size = 15
+
+    sets = IrisInConfigurationSpaceFromCliqueCover(
+        checker=checker, options=options, sets=[]
+    )
+
+    print(f"sets: {sets}")
+    print(type(sets))
+
+    test_iris_region(plant, plant_context, meshcat, sets)
     
